827efb45b2c9a79df310e16e9ff753070e1c1878
thesiya-nikunj <wd.nikunj@gmail.com>
2021-07-02 15:23:40 +0530

    Designd Post done

--- pages/components/PostComment.js
+++ pages/components/PostComment.js
@@ -1,47 +1,31 @@
 import Image from "next/image";
 import React, { useState } from "react";
+import ReactReadMoreReadLess from "react-read-more-read-less";
 import Profile from "../../images/profile.jpg";
-
-const ReadMore = ({ children }) => {
-	const text = children;
-	const [isReadMore, setIsReadMore] = useState(true);
-	const toggleReadMore = () => {
-		setIsReadMore(!isReadMore);
-	};
-	return (
-		<p className="text">
-			{isReadMore ? text.slice(0, 150) : text}
-			<span
-				onClick={toggleReadMore}
-				className="read-or-hide text-blue-500 hover:underline cursor-pointer"
-			>
-				{text.length >= 150
-					? isReadMore
-						? "...read more"
-						: " show less"
-					: null}
-			</span>
-		</p>
-	);
-};
 
 const PostComment = () => {
 	return (
-		<div className="flex space-x-4">
-			<div className="h-24 w-24 lg:w-12 lg:h-12 rounded-full overflow-hidden">
-				<Image
-					src={Profile}
-					alt="comment"
-					className="object-cover rounded-full"
-				/>
+		<div className="flex space-x-4 justify-between">
+			<div className="">
+				<div className="h-8 w-8 lg:w-10 lg:h-10 rounded-full overflow-hidden">
+					<Image
+						src={Profile}
+						alt="comment"
+						className="object-cover rounded-full"
+					/>
+				</div>
 			</div>
-			<div className="flex flex-col items-start justify-start">
+			<div className="w-11/12 flex flex-col items-start justify-start">
 				<h4 className="text-sm sm:text-base font-medium">
 					ll_nikunj.thesiya_ll
 				</h4>
 
 				<p className="text-left text-xs sm:text-sm mt-1">
-					<ReadMore>
+					<ReactReadMoreReadLess
+						charLimit={150}
+						readMoreText={"Read more ▼"}
+						readLessText={"Read less ▲"}
+					>
 						A Computer Science portal for geeks. It contains well written, well
 						thought and well explained computer science, programming articles
 						and quizzes. It provides a variety of services for you to learn, so
@@ -49,7 +33,7 @@ const PostComment = () => {
 						Live, Online and Classroom Courses ,Frequent Coding Competitions,
 						Webinars by Industry Experts, Internship opportunities, and Job
 						Opportunities. Knowledge is power!
-					</ReadMore>
+					</ReactReadMoreReadLess>
 				</p>
 
 				<p className="text-left text-xs mt-2 text-gray-500 ">2 hours ago..</p>
