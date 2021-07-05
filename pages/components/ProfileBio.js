79e304fc1546823971e2464e15f9e050a5549cc2
thesiya-nikunj <wd.nikunj@gmail.com>
2021-07-05 14:03:08 +0530

    Profile page created.

--- pages/components/ProfileBio.js
+++ pages/components/ProfileBio.js
@@ -4,8 +4,29 @@ import ScrollContainer from "react-indiana-drag-scroll";
 
 const ProfileBio = () => {
 	return (
-		<div className="flex items-center justify-center w-full h-screen">
-			<h1 className="text-center text-3xl font-bold">Under Construction</h1>
+		<div className="flex w-3/4 flex-col items-center justify-center mt-4 mb-24">
+			<div className="flex flex-col md:flex-row px-12 md:px-8 py-8 items-center justify-center">
+				<Image
+					src={Profile}
+					className="object-cover rounded-3xl"
+					width={180}
+					height={180}
+					quality={100}
+					objectFit="cover"
+				/>
+			</div>
+			<div className="flex flex-col items-center space-y-3">
+				<h1 className="text-3xl md:text-4xl text-dc-blue text-center font-bold">
+					Nikunj Thesiya
+				</h1>
+				<h1 className="md:text-xl text-gray-500 text-center font-light">
+					Front End Developer at Microsoft.
+				</h1>
+				<h1 className="md:text-xl text-gray-500 text-center font-light">
+					From Gujarat, India
+				</h1>
+			</div>
+			<span className="btn-sm mt-5">Edit Profile</span>
 		</div>
 	);
 };
