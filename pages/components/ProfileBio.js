fd8b17130a6def91c1220ed329b0d5bb11772812
thesiya-nikunj <wd.nikunj@gmail.com>
2021-07-06 11:18:35 +0530

    Social Media Component Added to Users Profile.

--- pages/components/ProfileBio.js
+++ pages/components/ProfileBio.js
@@ -2,6 +2,10 @@ import Image from "next/image";
 import Profile from "../../images/profile.jpg";
 import ScrollContainer from "react-indiana-drag-scroll";
 import Github from "../../images/github.svg";
+import Instagram from "../../images/instagram.svg";
+import Facebook from "../../images/facebook.svg";
+import Twitter from "../../images/twitter.svg";
+import LinkedIn from "../../images/linkedin.svg";
 import { AiOutlineStar, AiOutlineEye } from "react-icons/ai";
 import { VscRepoForked } from "react-icons/vsc";
 
@@ -28,6 +32,60 @@ const ProfileBio = () => {
 				<h1 className="md:text-xl text-gray-500 text-center font-light">
 					From Gujarat, India
 				</h1>
+			</div>
+
+			{/* Social Media */}
+			<div className="flex space-x-4 mt-5">
+				<span className="social-link">
+					<Image
+						src={Facebook}
+						className="object-cover"
+						width={30}
+						height={30}
+						quality={100}
+						objectFit="cover"
+					/>
+				</span>
+				<span className="social-link">
+					<Image
+						src={Instagram}
+						className="object-cover"
+						width={30}
+						height={30}
+						quality={100}
+						objectFit="cover"
+					/>
+				</span>
+				<span className="social-link">
+					<Image
+						src={Twitter}
+						className="object-cover"
+						width={30}
+						height={30}
+						quality={100}
+						objectFit="cover"
+					/>
+				</span>
+				<span className="social-link">
+					<Image
+						src={Github}
+						className="object-cover"
+						width={30}
+						height={30}
+						quality={100}
+						objectFit="cover"
+					/>
+				</span>
+				<span className="social-link">
+					<Image
+						src={LinkedIn}
+						className="object-cover"
+						width={30}
+						height={30}
+						quality={100}
+						objectFit="cover"
+					/>
+				</span>
 			</div>
 			<div className="flex items-center mt-5 space-x-4">
 				<div className="flex flex-col rounded-2xl text-gray-500 bg-dc-gray py-4 px-6 items-center text-center border border-transparent hover:border-dc-blue hover:border-opacity-10 cursor-pointer">
